4b27a575b2fe6618f03c2dd68c2de6f62615ce42
Beisenbek M. Baisakov <b.baisakov@kbtu.kz>
2024-09-30 10:06:51 +0500

    first commit

---
+++ week1/5.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+
+    double a, b;
+    cin >> a >> b;
+
+    printf("%.2f %.3f",sqrt(a), sqrt(b));
+    
+    return 0;
+}
---
+++ week1/7.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+
+    char c;
+    cin >> c;
+    cout << char(int(c) + 1);
+
+    return 0;
+}
